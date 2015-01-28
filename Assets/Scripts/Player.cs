cfad5b3f83267dd26b5281ba340cec66f9d56868
Hanson Chun <hansonchun@gmail.com>
2015-01-27 22:46:32 -0800

    Added movement physics
    - move done
    - grounded layer done
    - need to work on variable jump height

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	// Movement mechanics
+	public int speed;
+	public float horizontalMovement = 0;
+	public float verticalMovement = 0;
+
+	// Jump mechanics
+	public int jumpSpeed;
+	public int jumpSpeedShort;
+	public bool inJump = false;
+	public bool cancelJump = false;
+
+	// Grounded mechanics
+	public bool isGrounded;
+	public Transform pointOne;
+	public Transform pointTwo;
+	public LayerMask onlyGroundMask;
+
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		isGrounded = Physics2D.OverlapArea(pointOne.position, pointTwo.position, onlyGroundMask);
+
+		if (Input.GetKeyDown (KeyCode.Space) && isGrounded) {
+			inJump = true;
+		}
+
+		if (Input.GetKeyUp (KeyCode.Space) && !isGrounded) {
+			cancelJump = true;
+		}
+		  
+	}
+	
+
+	void FixedUpdate() {
+
+		horizontalMovement = Input.GetAxis ("Horizontal");
+		verticalMovement = rigidbody2D.velocity.y;
+
+		// movements
+		rigidbody2D.velocity = new Vector2 (horizontalMovement * speed * Time.deltaTime, verticalMovement);
+
+		// jumps
+		if (inJump) {
+			rigidbody2D.AddForce (new Vector2 (0, jumpSpeed * Time.deltaTime), ForceMode2D.Impulse);
+			inJump = false;
+			}
+
+		if (cancelJump) {
+				rigidbody2D.AddForce (new Vector2 (0, jumpSpeedShort * Time.deltaTime), ForceMode2D.Impulse);
+			cancelJump = false;
+			}
+	}
+	
+
+	
+}
+
+
