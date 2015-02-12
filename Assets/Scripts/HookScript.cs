7905207e4fb0e86f8076629487798a60df6d12fc
Hanson Chun <hansonchun@gmail.com>
2015-02-12 01:20:29 -0800

    fixed up grapple
    - grapple now shoots out and hangs

---
+++ Assets/Scripts/HookScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HookScript : MonoBehaviour {
+
+	public Transform player;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollision2D(Collision2D other) 
+	{
+		if (other.gameObject.tag == "Pullable") 
+		{
+			rigidbody2D.isKinematic = true;
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerGrappleShoot.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerGrappleShoot : MonoBehaviour 
+{
+	public Transform grappleStart;
+	public Transform grappleEnd;
+	
+	public GameObject hook;
+	
+	public bool pullable = false;
+
+	public bool isHooked;
+	
+	public SpringJoint2D springJoint;
+
+	// Use this for initialization
+	void Start () 
+	{
+		springJoint = GetComponent<SpringJoint2D> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Raycasting ();
+		Grapple ();
+		CheckHooked ();
+		
+	}
+
+	void Raycasting() 
+	{
+		Debug.DrawLine (grappleStart.position, grappleEnd.position, Color.red);
+		pullable = Physics2D.Linecast (grappleStart.position, grappleEnd.position, 1 << LayerMask.NameToLayer("Pullable"));	
+	}
+
+	void Grapple()
+	{
+		if (Input.GetKeyDown (KeyCode.LeftShift) ) 
+		{
+			hook.transform.position = grappleEnd.transform.position;
+				if (pullable) 
+				{
+					isHooked = true;
+				}
+		}
+		
+	}
+
+	void CheckHooked() 
+	{
+		if (!isHooked) 
+		{
+			hook.transform.position = transform.position;
+		}
+	}
+
+
+}
