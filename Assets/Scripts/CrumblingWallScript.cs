cebf4e7b084b24f7229f9328e2ae9a1963466018
Hanson Chun <hansonchun@gmail.com>
2015-03-01 17:43:56 -0800

    added crumbling wall and blimp sprite

---
+++ Assets/Scripts/CrumblingWallScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrumblingWallScript : MonoBehaviour {
+
+	public float destroyTime;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	void OnCollisionEnter2D (Collision2D other) 
+	{
+		if (other.gameObject.tag == "Player") 
+		{
+			Invoke ("DestroyWall", destroyTime * Time.deltaTime);
+		}
+	}
+	
+	// Update is called once per frame
+
+
+	void DestroyWall () 
+	{
+		Destroy (gameObject);
+		Debug.Log ("Wall crumbling");
+	}
+}
---
+++ Assets/Scripts/GuardLightScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuardLightScript : MonoBehaviour {
+
+	public GameObject RightLight;
+
+	public Enemy enemy;
+
+	// Use this for initialization
+	void Start () {
+
+		enemy = GetComponentInParent <Enemy> ();
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		FlipLight ();
+	
+	}
+
+	void FlipLight()
+	{
+		if (enemy.facingRight) 
+		{
+			//transform = RightLight.transform;
+
+		}
+
+	}
+}
